a93684fe1c1777d31f1e262b90f2b57ac7fd2306
Zubayer <zubayerabm47@gmail.com>
2021-07-03 12:30:21 +0600

    Fix Navbar and Update the repository.

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
 export default function Nav() {
@@ -5,7 +6,13 @@ export default function Nav() {
     <header className="bg-gray-800 px-4 py-3 text-white flex justify-between items-center fixed w-full">
       <h1 className="font-semibold tracking-wider">TAILWINDCSS</h1>
 
-      <div className='md:hidden'>
+      <ul className="uppercas md:flex md:gap-3 hidden">
+        <li><a className='hover:text-gray-300' href="#">Documentation</a></li>
+        <li><a className='hover:text-gray-300' href="#">Components</a></li>
+        <li><a className='hover:text-gray-300' href="#">Example Pages</a></li>
+      </ul>
+
+      <div className="md:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
@@ -21,7 +28,6 @@ export default function Nav() {
           />
         </svg>
       </div>
-
     </header>
   );
 }
