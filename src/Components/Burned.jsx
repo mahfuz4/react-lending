4020d159b8151943585fc29a3f7b34db0a433b98
A B M Zubayer <zubayerabm47@gmail.com>
2021-07-13 14:26:37 +0000

    Another

--- src/Components/Burned.jsx
+++ src/Components/Burned.jsx
@@ -2,27 +2,6 @@ import React from 'react'
 import Cards from './Cards'
 import H1 from './ReusableComp/H1'
 
-const cards = [
-   {
-      id: 1,
-      title: 'Awarded Agency',
-      subtitle: 'Divide details about your product or agency work into parts. A paragraph describing a feature will be enough.',
-      className: 'bg-red-500'
-   },
-   {
-      id: 2,
-      title: 'Free Revisions',
-      subtitle: 'Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious.',
-      className: 'bg-blue-500'
-   },
-   {
-      id: 3,
-      title: 'Verified Company',
-      subtitle: 'Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!',
-      className: 'bg-green-500'
-   }
-]
-
 export default function Burned() {
    return (
       <>
@@ -32,10 +11,8 @@ export default function Burned() {
             <p className='text-gray-400 text-lg'>I've fought hard to get out of the hole I was in. I've dusted myself, and exactly like dancer, I have danced with the fear, you should have filmed me.</p>
          </div>
 
-         <div className='grid md:grid-cols-3 gap-8 max-w-7xl mx-auto px-5'>
-            {cards.map(({ id, className, title, subtitle }) => (
-               <Cards key={id} unique={id} className={className} title={title} subtitle={subtitle} />
-            ))}
+         <div className='grid md:grid-cols-3 gap-8 md:max-w-3xl lg:max-w-5xl mx-auto px-5'>
+            <Cards />
          </div>
       </>
    )
--- src/Components/Employee/Employe.jsx
+++ src/Components/Employee/Employe.jsx
@@ -1,47 +1,8 @@
 import React from 'react'
-import Img from '../../image/left.jpeg'
 import H1 from '../ReusableComp/H1'
 import P from '../ReusableComp/P'
-import Card from './Card'
+import Cards from './Cards'
 
-const cardDetails = [
-   {
-      id: 1,
-      src: Img,
-      name: 'A B M Zubayer',
-      title: 'web developer',
-      twitter: 'zubayer-dev',
-      facebook: 'zubayer-dev',
-      github: 'zubayer-47'
-   },
-   {
-      id: 2,
-      src: Img,
-      name: 'A B M Zubayer',
-      title: 'web developer',
-      twitter: 'zubayer-dev',
-      facebook: 'zubayer-dev',
-      github: 'zubayer-47'
-   },
-   {
-      id: 3,
-      src: Img,
-      name: 'A B M Zubayer',
-      title: 'web developer',
-      twitter: 'zubayer-dev',
-      facebook: 'zubayer-dev',
-      github: 'zubayer-47'
-   },
-   {
-      id: 4,
-      src: Img,
-      name: 'A B M Zubayer',
-      title: 'web developer',
-      twitter: 'zubayer-dev',
-      facebook: 'zubayer-dev',
-      github: 'zubayer-47'
-   }
-]
 
 export default function Employe() {
    return (
@@ -52,12 +13,7 @@ export default function Employe() {
             <H1 title="It is everything, and it won't change" className='text-3xl text-gray-700' />
             <P text='It depends on your friends and companions, it depends on who goes out with you, if they have money, if you have mone' className='text-gray-400 text-lg' />
          </div>
-
-         <div className='grid grid-row-1 grid-cols-1 md:grid-rows-2 md:grid-cols-2 lg:grid-rows-1 lg:grid-cols-4'>
-            {cardDetails.map(detail => (
-               <Card key={detail.id} src={detail.src} name={detail.name} title={detail.title} />
-            ))}
-         </div>
+         <Cards />
       </section>
    )
 }
