2dcd01a43cfef2ba0b03ad422f8aab3eae625ba8
MAHFUZ ANAM <mahfuz.anam.ma@gmail.com>
2021-07-13 10:46:24 +0600

    footer & last sections tiney change

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -3,10 +3,12 @@ import React from "react";
 export default function Input() {
   return (
     <section className="py-28 bg-gray-900 text-white sm-css md:md-css ">
-      <div className="text-center w-full lg:w-8/12 mx-auto mb-14">
-        <button className="uppercase text-green-600 text-xs px-2 py-1 rounded bg-green-300 font-semibold">
+      <div className="text-center w-full  lg:w-8/12 mx-auto mb-14">
+        <span 
+          className="select-none uppercase text-green-600 text-xs px-2 py-1 rounded bg-green-300 font-semibold"
+        >
           message us
-        </button>
+        </span>
         <h1 className="font-bold text-xl lg:text-3xl my-2 leading-relaxed">
           Type in the bellow form
         </h1>
@@ -23,20 +25,28 @@ export default function Input() {
         <p className="text-gray-600 text-lg mb-8">
           Complete this form and we will get back to you in 24 hours.
         </p>
-
-        <label className="uppercase tracking-wider leading-relaxed text-xs font-bold ">
+        <form>
+        <label
+          htmlFor="name"
+          className="uppercase tracking-wider leading-relaxed text-xs font-bold "
+        >
           full name
         </label>
         <input
+          id="name"
           className="mt-2 px-3 py-3 placeholder-blueGray-400 text-blueGray-500 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 border-transparent mb-3 "
           placeholder="First Name"
           type="text"
         />
 
-        <label className="uppercase tracking-wider leading-relaxed text-xs font-bold ">
+        <label
+          htmlFor="email"
+          className="uppercase tracking-wider leading-relaxed text-xs font-bold "
+        >
           email
         </label>
         <input
+          id="email"
           className="my-2 px-3 py-3 placeholder-blueGray-400 text-blueGray-500 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 border-transparent "
           placeholder="email"
           type="text"
@@ -53,10 +63,15 @@ export default function Input() {
           autocomplete="off"
         ></textarea>
         <div className="text-center">
-          <button className="mt-6 text-xs px-4 py-2 shadow hover:shadow-md  bg-gray-800 border-gray-800 active:bg-gray-900 active:border-blueGray-900 rounded outline-none focus:outline-none align-middle transition-all duration-150 ease-in-out uppercase border border-solid font-bold text-white">
+          <button
+            className="mt-6 text-xs px-4 py-2 shadow hover:shadow-md  bg-gray-800 border-gray-800 active:bg-gray-900 active:border-blueGray-900 rounded outline-none focus:outline-none align-middle transition-all duration-150 ease-in-out uppercase border border-solid font-bold text-white"
+            type="submit"
+          >
             send message
           </button>
+          
         </div>
+        </form>
       </div>
     </section>
   );
