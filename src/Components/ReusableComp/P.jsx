ff8a2455a8fcaa36881a49873f9ea63d161d1b68
A B M Zubayer <zubayerabm47@gmail.com>
2021-07-13 04:04:23 +0000

    Add few new born components.

--- src/Components/ReusableComp/P.jsx
+++ src/Components/ReusableComp/P.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function P({ className, text, weight }) {
    return (
-      <p className={`${className} text-gray-${weight} my-3`}>{text}</p>
+      <p className={`${className} text-gray-${weight} my-3 leading-7`}>{text}</p>
    )
 }
