ae791b7b8c4f60af76dad354dfeb1c92504fba51
MAHFUZ ANAM <mahfuz.anam.ma@gmail.com>
2021-07-13 04:06:40 +0000

    Update Home.jsx

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,16 +1,12 @@
-<<<<<<< src/Components/Pages/Home.jsx
+
 import React from 'react'
 import Burned from '../Burned'
 import CompanyDetails from '../CompanyDetails/CompanyDetails'
 import Employe from '../Employee/Employe'
 import Hero from '../Hero'
 import Nav from '../Nav/Nav'
-
-
 import Footer from "../Footer/Footer";
-
 import Input from "../Input/Input";
-
 import WorkingUs from "../WorkingWithUs/WorkingUs";
 
 export default function Home() {
@@ -18,8 +14,7 @@ export default function Home() {
     <>
       <Nav />
       <div className="bg-blue-50 pb-10 sm-css">
-        <Hero /><CompanyDetails />
-
+        <Hero />
         
         <Burned />
         <WorkingUs />
